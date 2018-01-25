0508ca624a5ef0e2fa0d220e13cc6e2881a8997e
Artorp <Artorp@users.noreply.github.com>
2018-01-25 11:49:15 +0100

    Non-static load, new title

--- src/dotsystem/Main.java
+++ src/dotsystem/Main.java
@@ -11,14 +11,16 @@ import javafx.stage.Stage;
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("DotSystem.fxml"));
 		Parent root;
 		try {
-			root = FXMLLoader.load(getClass().getResource("DotSystem.fxml"));
+			root = loader.load();
 		} catch (IOException e) {
 			e.printStackTrace();
 			return;
 		}
 		Scene scene = new Scene(root);
+		primaryStage.setTitle("Prikkesystem");
 		primaryStage.setScene(scene);
 		primaryStage.sizeToScene();
 		primaryStage.show();
